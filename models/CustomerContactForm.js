70b55848043630a26c610894458fb0f1933fdfa5
varun <VarunSureshkumarPatel07@gmail.com>
2024-02-24 23:30:51 +0530

    created api for tacking customer info for their inquiry

--- models/CustomerContactForm.js
+++ models/CustomerContactForm.js
@@ -1,35 +1,36 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const CustomerContactFormSchema = new Schema({
-  firstname: {
-    type: String,
-    required: true,
+const CustomerContactFormSchema = new Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    interestedin: {
+      type: Array,
+    },
+    number: {
+      type: Number,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
   },
-  lastname: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  interestedin: {
-    type: Array,
-  },
-  number: {
-    type: Number,
-    required: true,
-  },
-  message: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const CustomerContactForm = mongoose.model(
   "customerContactForm",
--- routes/customerContactInfo.js
+++ routes/customerContactInfo.js
@@ -5,11 +5,11 @@ const { body, validationResult } = require("express-validator");
 const CustomerContactForm = require("../models/CustomerContactForm");
 const MailTransport = require("../helper/SendMails");
 const { VerificationEmailBody } = require("../helper/VerificationMailBody");
-
+const fetchusers = require("../middleware/fetchusers");
+let success = false;
 // ? THIS ROUTES IS USED TO GET THE INFO OF THE CUSTOMER WHO WANTED TO CONTACT US  FROM LANDING PAGE AND STORE IT TO OUR DATABASE AND ALSO  SEND THE EMAIL TO THE CONSUMER THAT THEIR INFO IS SUMBITED AND WE WILL MAKE A CONTACT TO YOU
-  
-//? TO TEST THE API ====>{"http://localhost:500/app/api/customerContactInfo/registercustomer"}
 
+//? TO TEST THE API ====>{"http://localhost:500/app/api/customerContactInfo/registercustomer"}
 
 routes.post(
   "/registercustomer",
@@ -18,7 +18,7 @@ routes.post(
     body("lastname", "the length of the lastname is etlist 3").exists(),
     body("email", "email is invalid").isEmail(),
     body("number", "number is invalid").exists(),
-    body("message", "the length of the message is etlist 3 ").exists()
+    body("message", "the length of the message is etlist 3 ").exists(),
   ],
   async (req, res) => {
     try {
@@ -26,18 +26,19 @@ routes.post(
       if (!result.isEmpty()) {
         return res.json({ result });
       }
-      const {firstname , lastname , email ,interestedin, number , message ,} = req.body
+      const { firstname, lastname, email, interestedin, number, message } =
+        req.body;
       // now we will take the information from the user and save it in the database and then send the email to the users
       let customerInfo = new CustomerContactForm({
-        firstname:firstname,
-        lastname:lastname,
-        email:email,
-        interestedin:interestedin,
-        number:number,
-        message:message
-      })
-      const FullName = firstname +" "+lastname
-     console.log(FullName)
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        interestedin: interestedin,
+        number: number,
+        message: message,
+      });
+      const FullName = firstname + " " + lastname;
+      console.log(FullName);
       try {
         MailTransport.sendMail({
           from: process.env.EMAIL_ADDRESS_OF_AUTHER,
@@ -46,10 +47,13 @@ routes.post(
           html: VerificationEmailBody(4545),
         });
       } catch (error) {
-        res.json({message:"there is an error sending an email" , error:error})
+        res.json({
+          message: "there is an error sending an email",
+          error: error,
+        });
       }
-      await customerInfo.save()
-      res.json({customerInfo})
+      await customerInfo.save();
+      res.json({ customerInfo });
     } catch (error) {
       res.json({
         message: "there is an error to get your data plese try again letter",
@@ -57,5 +61,25 @@ routes.post(
     }
   }
 );
+routes.get("/GetAllCustomer", fetchusers, async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 2; // Default to page 1 if not provided
+    const pageSize = parseInt(req.query.pageSize) || 10; // Default page size to 10 if not provided
+    const All_Customer = await CustomerContactForm.find().sort({
+      updatedAt: -1,
+    });
+    console.log(page)
+    const startIndex = (page - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    const Filtered_Data = All_Customer.slice(startIndex, endIndex);
+    success = true;
+    res.json({ success, Filtered_Data });
+  } catch (error) {
+    res.json({
+      success,
+      message: "the error accrued in the get all customer function",
+    });
+  }
+});
 
 module.exports = routes;
